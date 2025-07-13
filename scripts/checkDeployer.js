d4f2ae50a5d7cbbeccb1f2130e9e6080f0b17b70
kazol196295 <kazol1907063@stud.kuet.ac.bd>
2025-07-13 16:13:05 +0600

    frontend

---
+++ scripts/checkDeployer.js
@@ -0,0 +1,14 @@
+// scripts/checkDeployer.js
+const hre = require("hardhat");
+
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+  console.log("Deployer address:", deployer.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
